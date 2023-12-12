d49c884cb8543420d1c0ee5394b691741722cdd8
trevb11 <143556959+trevb11@users.noreply.github.com>
2023-12-12 01:09:44 -0800

    Update test_PCA599.R

--- tests/test_PCA599.R
+++ tests/test_PCA599.R
@@ -3,10 +3,10 @@ library(devtools)
 install.packages('stats')
 
 
-test_that("PCA computs same answer as R", {
+test_that("PCA599 computs same answer as R", {
   set.seed(17)
   test.data <- matrix(rnorm(300), ncol = 2)
-  my.result <- PCA(test.data, 2)
+  my.result <- PCA599(test.data, 2)
   expected.result <- stats::prcomp(test.data)
   expect_equal(my.result, expected.result)
 })
